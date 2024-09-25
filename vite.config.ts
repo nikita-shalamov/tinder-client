835bf687530b84dd3245791d40d1e0b8c1db7f37
nikita-shalamov <nikita.shalamovv@yandex.ru>
2024-09-25 17:06:30 +0500

    changed babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import babel from 'vite-plugin-babel';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      babelConfig: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
