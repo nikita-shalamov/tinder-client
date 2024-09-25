e1ce881197dc9fb872fb32752021a1556101ab34
nikita-shalamov <nikita.shalamovv@yandex.ru>
2024-09-25 17:02:05 +0500

    add swipes for profiles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import babel from 'vite-plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    babel({
+      babelConfig: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+      },
+    }),
+  ],
+});
